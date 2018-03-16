9b247f9c771d38a4546f0c383bfe215d6b75e3bd
diddydum <37198933+diddydum@users.noreply.github.com>
2018-03-15 18:05:05 -0700

    Add in a terrible example that just repeats what messages you send

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  websocket: WebSocket;
+  messages = ['Message 1', 'Message 2'];
+  command = '';
+
+  ngOnInit() {
+    // TODO: put ws stuff in its own class
+    this.websocket = new WebSocket('ws://localhost:8080/ws');
+    this.websocket.onclose = () => console.log('DEBUG onclose occurred');
+    this.websocket.onopen = () => console.log('DEBUG onopen occurred');
+    this.websocket.onerror = () => console.log('DEBUG onerror occurred');
+    this.websocket.onmessage = (event) => {
+      console.log('DEBUG onmessage occurred');
+      this.messages.push(event.data);
+    };
+  }
+
+  onCommandSubmit() {
+    this.websocket.send(this.command);
+
+    this.command = '';
+  }
+
+  isConnected() {
+    return this.websocket && this.websocket.readyState === WebSocket.OPEN;
+  }
 }
